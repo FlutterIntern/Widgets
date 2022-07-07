323fb50a11757716d2f7a500b715dd31809cae1b
Muhammad Saqib <224saqib@gmail.com>
2022-07-07 05:16:21 -0700

    saqib

---
+++ TopAppBar/topAppBar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:travel_guide/TravelerView/MainMenu/mainMenu.dart';
+import 'package:travel_guide/TravelerView/UserProfile/userProfile.dart';
+class TopAppBar extends StatelessWidget {
+  const TopAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18.0,vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            onTap: (){
+              Navigator.of(context).pop();
+            },
+            child: ImageIcon(
+
+                AssetImage("assets/Icons/Vector (13).png")),
+          ),
+          InkWell(
+            onTap: (){Get.to(MainMenu());},
+            child: CircleAvatar(
+              radius: 22,
+              backgroundImage: AssetImage("assets/images/Ellipse 1.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
